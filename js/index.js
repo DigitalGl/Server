5d6f96ac07d3ba2ea8d7089e5116a72f7ac336c6
Daniil <daniil424342@gmail.com>
2024-09-07 14:35:42 +0500

    new commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+let lisStudent = [
+    {
+        name: 'Илья',
+        lastname: 'Иванов',
+        surename: 'Олегович',
+        birthday: new Date(1994, 5, 12),
+        faculty: 'Экономика',
+        start: 2010,
+    },
+    {
+        name: 'Оля',
+        lastname: 'Студентова',
+        surename: 'Александровна',
+        birthday: new Date(1991, 11, 18),
+        faculty: 'Экономика',
+        start: 2011,
+    },
+    {
+        name: 'Татьяна',
+        lastname: 'Иванова',
+        surename: 'Олеговина',
+        birthday: new Date(1997, 4, 1),
+        faculty: 'Информатика',
+        start: 2016,
+    },
+]
+
+
+console.log(lisStudent);
+
+
+
+
+
+
