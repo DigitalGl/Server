1fd5789c7a58513f721f6fcc884863663c8718c3
Daniil <daniil424342@gmail.com>
2024-09-11 15:00:56 +0500

    new commit

--- js/index.js
+++ js/index.js
@@ -26,17 +26,57 @@ let lisStudent = [
 ]
 
 
-const $studeTable = document.getElementById('stud-table')
 
-for (const studObj of lisStudent) {
-    const $tr = document.createElement("tr")
-    const $tdFIO = document.createElement("td")
-    const $tdBirthday = document.createElement("tr")
-    const $tdFaculty = document.createElement("tr")
-    const $tdStart = document.createElement("tr")
+function formatDate(date) {
 
-    $tr.append($tdFIO, $tdBirthday, $tdFaculty, $tdStart)
-    $studeTable.append($tr)
+    var dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
+
+    var mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+
+    var yy = date.getFullYear() % 100;
+    if (yy < 10) yy = '0' + yy;
+
+    return dd + '.' + mm + '.' + yy;
 }
 
 
+
+
+
+function $getNewStudentTR(studObj){
+    const $tr = document.createElement("tr")
+    const $tdFIO = document.createElement("td")
+    const $tdBirthday = document.createElement("td")
+    const $tdFaculty = document.createElement("td")
+    const $tdStart = document.createElement("td")
+
+    $tdFIO.textContent = `${studObj.lastname} ${studObj.name} ${studObj.surename}`
+    $tdBirthday.textContent = formatDate(studObj.birthday)
+    $tdFaculty.textContent = studObj.faculty
+    $tdStart.textContent = studObj.start
+
+
+    $tr.append($tdFIO, $tdBirthday, $tdFaculty, $tdStart)
+    return $tr
+}
+
+
+
+
+
+function render(arr){
+    let copyArr = [...arr]
+
+    const $studeTable = document.getElementById('stud-table')
+    $studeTable.innerHTML = ''
+
+for (const studObj of copyArr) {
+    const $newTr = $getNewStudentTR(studObj)
+
+    $studeTable.append($newTr)
+}
+}
+
+render(lisStudent)
